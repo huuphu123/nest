ffbe0707b7e89b26e7df2761ebe30ce7d514fa5a
Phu <phamhphu22@gmail.com>
2023-04-03 20:37:04 +0700

    new

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,5 @@
 import { Controller, Get } from '@nestjs/common';
+import { ApiResponse} from '@nestjs/swagger';
 import { AppService } from './app.service';
 
 @Controller()
@@ -6,6 +7,7 @@ export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
+  @ApiResponse({ status: 200, description: 'Hello World!'})
   getHello(): string {
     return this.appService.getHello();
   }
--- src/runs/runs.controller.ts
+++ src/runs/runs.controller.ts
@@ -1,5 +1,5 @@
 import { Controller, Get, Param, Post, Put, Body, Delete, Query } from '@nestjs/common';
-import { ApiProperty, ApiTags, ApiOkResponse, ApiCreatedResponse} from '@nestjs/swagger';
+import { ApiProperty, ApiTags, ApiOkResponse, ApiCreatedResponse, ApiQuery, ApiResponse} from '@nestjs/swagger';
 import { RunsService } from './runs.service';
 import { CreateRunsDto, UpdateRunsDto} from './data.dto';
 
@@ -9,35 +9,51 @@ export class RunsController {
   constructor(private readonly runsService: RunsService) {}
 
     @Get()
-    @ApiOkResponse({description: 'Get all history successfully!'})
+    @ApiOkResponse({description: 'Get all history successfully!', type: CreateRunsDto, isArray: true})
     async getRuns() {
         const runs = await this.runsService.getRuns();
         return runs;
     }
 
-    @Get(':id')
-    async getRun(@Param('id') id) {
-        const runs = await this.runsService.getRun(id);
-        return runs;
-    }
-
+    
     @Post()
     @ApiCreatedResponse({
         description: 'The record has been successfully created.',
         type: CreateRunsDto,
+        isArray: true
     })
+    @ApiResponse({ status: 404, description: 'Id has been existed!'})
     async addRuns(@Body() CreateRunsDto: CreateRunsDto) {
         const runs = await this.runsService.addRuns(CreateRunsDto);
         return runs;
     }
+    
+    @Get(':id')
+    @ApiOkResponse({ description: 'Get a Runs history by id', type: CreateRunsDto })
+    async getRun(@Param('id') id: string) {
+        const runs = await this.runsService.getRun(id);
+        return runs;
+    }
 
     @Put(':id')
+    @ApiCreatedResponse({
+        description: 'The record with id has been updated successfully!',
+        type: CreateRunsDto, 
+        isArray: true
+    })
+    @ApiResponse({ status: 404, description: 'Runs information not found!'})
     async updateRuns(@Param('id') id: string, @Body() UpdateRunsDto: UpdateRunsDto) {
         const runs = await this.runsService.updateRuns(id, UpdateRunsDto);
         return runs;
     }
 
     @Delete(':id')
+    @ApiCreatedResponse({
+        description: 'The record with id has been deleted successfully!',
+        type: CreateRunsDto, 
+        isArray: true
+    })
+    @ApiResponse({ status: 404, description: 'Runs information not found!'})
     async deleteRuns(@Param('id') id: string) {
         const runs = await this.runsService.deleteRuns(id);
         return runs;
